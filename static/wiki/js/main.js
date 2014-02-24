3925aeeec6d99425b60c9930c8e482da142f7583
davidjulrich@gmail.com <none@example.com>
2014-02-24 15:55:51 -0800

    Added some style

---
+++ static/wiki/js/main.js
@@ -0,0 +1,30 @@
+function Toggle(id) 
+{
+  e = document.getElementById(id).style;
+  e.display = ( e.display != 'block' ) ? 'block' : 'none';
+}
+
+function buttonToggle(where, showLabel, hideLabel, disp) {
+    if(typeof(disp)==='undefined') disp = 'block'
+    var e = document.getElementById(where.attributes.rel.value);
+    where.value = (where.value == showLabel) ? hideLabel : showLabel;
+    e.style.display = (e.style.display == disp) ? 'none' : disp;
+}
+
+function FindFocus()
+{
+  bFound = false;
+  for ( f = 0; f < document.forms.length; f++ ) 
+  {
+    for ( i = 0; i < document.forms[f].length; i++ ) 
+    {
+      if ( document.forms[f][i].type != "hidden" && document.forms[f][i].disabled != true )
+      { 
+        document.forms[f][i].focus(); 
+        bFound = true; 
+      }
+      if ( bFound == true ) break;
+    }
+    if ( bFound == true ) break;
+  }
+}
