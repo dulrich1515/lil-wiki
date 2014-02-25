fa7d554bfafd7b7a3a9b1a42af9e7077864bd7d4
dulrich15 <davidjulrich@gmail.com>
2014-02-24 23:21:38 -0800

    Update views.py

--- views.py
+++ views.py
@@ -16,7 +16,8 @@ def list_by_name(request):
     pg_walk = next(os.walk(pg_path))
     
     context = {
-        'pg_walk': pg_walk,
+        'dirs': sorted(pg_walk[1]),
+        'files': sorted(pg_walk[2]),
     }
     template = 'wiki/list_by_name.html'
     return render_to_response(request, template, context)
