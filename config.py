f3e673ffeda8d8036d22fdf89793b79072b7c547
davidjulrich@gmail.com <none@example.com>
2014-02-25 09:39:39 -0800

    Added templatetags

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import os
 
-pg_path = os.path.join('..', 'oldwiki', 'content')
+pg_path = os.path.join('..', 'wiki-pages')
 pg_path = os.path.join(os.path.dirname(os.path.abspath( __file__ )), pg_path)
 
 if not os.path.exists(pg_path):
