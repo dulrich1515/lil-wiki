7e630cca480181b972927391b5c3483a70ea5235
dulrich15 <davidjulrich@gmail.com>
2014-02-27 12:48:58 -0800

    Fixed small bug

--- views.py
+++ views.py
@@ -25,7 +25,7 @@ def wiki_logout(request):
 
 def list_by_name(request):
     pg_walk = next(os.walk(pg_path))
-    
+
     template = 'wiki/list_by_name.html'
     context = {
         'dirs': sorted(pg_walk[1]),
@@ -37,7 +37,7 @@ def list_by_name(request):
 def show(request, pg=''):
     if not pg:
         return list_by_name(request)
-    if pg[:1] = '/':
+    if pg[:1] == '/':
         return redirect('wiki_show', pg[:1])
 
     # page = Page.objects.get(pg)
@@ -53,12 +53,12 @@ def show(request, pg=''):
     return render_to_response(request, template, context)
 
 
-@login_required(login_url='/wiki/login/')    
-def edit(request, pg=''): 
+@login_required(login_url='/wiki/login/')
+def edit(request, pg=''):
 # blank will create a *new* page, but how to edit WikiRoot page?
     if not request.user.is_staff:
         return redirect('wiki_root')
- 
+
     # page = Page.objects.get(pg)
     page = Page(pg)
 
@@ -67,9 +67,9 @@ def edit(request, pg=''):
         'page' : page,
     }
     return render_to_response(request, template, context)
-    
-    
-def post(request): 
+
+
+def post(request):
     if request.user.is_staff and request.method == 'POST':
         pg = request.POST['pg']
         title = request.POST['title']
@@ -88,18 +88,18 @@ def post(request):
             if os.path.isfile(page.fp): # delete only files for now...
                 os.remove(page.fp)
             return redirect('wiki_root')
-        
+
         elif 'update' in request.POST or 'submit' in request.POST:
             if '/' in title:
-                check_path(title) 
+                check_path(title)
             page.save(content) # shouldn't check_path be in page.save?
-            
+
             if title.lower() != pg.lower(): # case-sensitivity issue here?
             # then title was changed ... need to delete old file
                 fp = os.path.join(pg_path, pg)
                 if os.path.isfile(fp):
                     os.remove(fp)
-                    
+
             if 'update' in request.POST:
                 return redirect('wiki_edit', title)
             else:
@@ -108,7 +108,7 @@ def post(request):
     # nothing should get here...
     return redirect('wiki_root')
 
-    
+
 def check_path(title):
     dirs = title.split('/')[:-1]
     if not os.path.isdir(os.path.join(pg_path, *dirs)):
