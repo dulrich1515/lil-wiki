587bcf2beace510bae6a3b48debcc336585eec63
Dave <none@example.com>
2014-03-16 22:42:46 -0700

    Small css tweak

---
+++ templatetags/docutils_extensions/matplotlib/temp.py
@@ -0,0 +1,31 @@
+from __future__ import division
+
+import numpy as np
+from numpy import sin, cos
+
+from scipy import integrate
+from scipy.fftpack import fft,ifft
+from scipy.spatial.distance import pdist, squareform
+
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+fig = plt.figure()
+
+ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+line, = ax.plot([], [], lw=5)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
+ani.save('temp.mp4', fps=30, extra_args=['-vcodec', 'libx264', '-pix_fmt', 'yuv420p'])
