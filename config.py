dc0b08e2bca19c4d07e5c4b60a1c4f71ec7b0928
Dave <davidjulrich@gmail.com>
2014-04-04 16:25:07 +0000

    Moved wiki-pages

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-﻿from __future__ import division
+from __future__ import division
 from __future__ import unicode_literals
 
 import os
 
-wiki_pages_path = os.path.join('..', 'wiki-pages')
+wiki_pages_path = os.path.join('..', '_', 'wiki-pages')
 wiki_pages_path = os.path.join(os.path.dirname(os.path.abspath( __file__ )), wiki_pages_path)
 
 if not os.path.exists(wiki_pages_path):
--- templatetags/docutils_extensions/__init__.py
+++ templatetags/docutils_extensions/__init__.py
@@ -1,4 +1,4 @@
-﻿from __future__ import division
+from __future__ import division
 from __future__ import unicode_literals
 
 import codecs
