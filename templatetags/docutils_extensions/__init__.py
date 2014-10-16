f73f9c203d889470da0892f297f87cd92cbf72d3
Dave <davidjulrich@gmail.com>
2014-10-16 16:12:14 +0000

    Automatic push from site

--- templatetags/docutils_extensions/__init__.py
+++ templatetags/docutils_extensions/__init__.py
@@ -1034,13 +1034,13 @@ class problem_set_directive(rst.Directive):
                 if numbering: 
                     text += '<li>\n'
                     
-                text += '<p>{}</p>\n'.format(q)
-
                 if answers == 'toggle':
                     text += '<input class="toggler" type="button" rel="a{}" value="Show Answer" onclick="buttonToggle(this,\'Show Answer\',\'Hide Answer\')">\n'.format(toggle_id)
                 if solutions == 'toggle':
                     text += '<input class="toggler" type="button" rel="s{}" value="Show Solution" onclick="buttonToggle(this,\'Show Solution\',\'Hide Solution\')">\n'.format(toggle_id)
                 
+                text += '<p>{}</p>\n'.format(q)
+
                 if answers == 'show':
                     text += '<p id="a{}">\n<i>Answer:</i> {}\n</p>\n'.format(toggle_id, a)
                 elif answers == 'toggle':
