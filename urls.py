181740f3282efb3db18452617caf261c9afb8ec8
Dave <davidjulrich@gmail.com>
2015-04-08 19:52:12 +0000

    Automatic push from site

--- urls.py
+++ urls.py
@@ -6,14 +6,14 @@ from django.views.generic.simple import redirect_to
 import views
 
 urlpatterns = patterns('',
-    url(r'^wiki/$', views.show, name='wiki_root'),
+    url(r'^$', views.show, name='wiki_root'),
 
-    url(r'^wiki/show(?P<pg>(/[\w\-/]*))$', views.show, name='wiki_show'),
-    url(r'^wiki/edit(?P<pg>(/[\w\-/]*))$', views.edit, name='wiki_edit'),
-    url(r'^wiki/ppdf(?P<pg>(/[\w\-/]*))$', views.ppdf, name='wiki_ppdf'),
+    url(r'^show(?P<pg>(/[\w\-/]*))$', views.show, name='wiki_show'),
+    url(r'^edit(?P<pg>(/[\w\-/]*))$', views.edit, name='wiki_edit'),
+    url(r'^ppdf(?P<pg>(/[\w\-/]*))$', views.ppdf, name='wiki_ppdf'),
 
-    url(r'^wiki/post/$', views.post, name='wiki_post'),
+    url(r'^post/$', views.post, name='wiki_post'),
 
-    url(r'^wiki/login/$', 'django.contrib.auth.views.login', {'template_name': 'wiki/login.html'}, name='wiki_login'),
-    url(r'^wiki/logout/$', views.wiki_logout, name='wiki_logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'wiki/login.html'}, name='wiki_login'),
+    url(r'^logout/$', views.wiki_logout, name='wiki_logout'),
 )
